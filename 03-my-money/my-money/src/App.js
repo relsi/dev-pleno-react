159f9a031072eadbe9698989c2a5a403f1e51c52
Relsi Maron <relsi@codeup.com.br>
2020-04-08 15:52:12 -0300

    Cria a funçao reducer para controle de estado

--- 03-my-money/my-money/src/App.js
+++ 03-my-money/my-money/src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState, useReducer} from 'react';
+import axios from 'axios'
+
+const url = 'https://mymoney-curso-react.firebaseio.com/movimentacoes/2020-04.json';
+
+const reducer = (state, action) => {
+
+  if (action.type === 'REQUEST') {
+    return {
+      ...state,
+      loading: true
+    }
+  }
+
+  if (action.type === 'SUCCESS') {
+    return {
+      ...state,
+      loading: false,
+      data: action.data
+    }
+  }
+
+  return state;
+
+}
 
 function App() {
+
+  const [data, dispatch] = useReducer(reducer, {
+    loading: true,
+    data: {}
+  });
+
+  useEffect(() => {
+    
+    dispatch({type: 'REQUEST'});
+
+    axios
+      .get(url)
+      .then(response => {
+        dispatch({type: 'SUCCESS', data: response.data});
+      });
+
+  }, []);
+
+  if (data.loading) {
+    return <p>Loading....</p>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MyMoney</h1>
+      
+      {JSON.stringify(data)}
     </div>
   );
 }

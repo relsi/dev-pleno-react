1d587bc9cfafaca69b4f06fa1be908dc973178ac
Relsi Maron <relsi@codeup.com.br>
2020-04-08 19:06:42 -0300

    cria o hook para salvar os dados

--- 03-my-money/my-money/src/App.js
+++ 03-my-money/my-money/src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import useGet from './useGet'
+import usePost from './usePost'
 
 const url = 'https://mymoney-curso-react.firebaseio.com/movimentacoes/2020-04.json';
 
 function App() {
 
   const data = useGet(url);
-  const data2 = useGet('http://httpbin.org/ip');
+  const [postData, post] = usePost(url);
+
+
+
+  const saveNew = () => {
+    post({valor:10, descricao: 'olá'});
+  }
 
   if (data.loading) {
     return <p>Loading....</p>
   }
+
   return (
     <div>
       <h1>MyMoney</h1>
-      {JSON.stringify(data)}
-      <pre>{JSON.stringify(data2)}
-      </pre>
+
+      <p>{JSON.stringify(data)}</p>
+      <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={saveNew}>Novo</button>
     </div>
   );
 }

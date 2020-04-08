ad5f4479e5b675482d018220b1faebc18611e7db
Relsi Maron <relsi@codeup.com.br>
2020-04-08 16:36:26 -0300

    desmembra o componente para a reutilizaçao com hooks

--- 03-my-money/my-money/src/App.js
+++ 03-my-money/my-money/src/App.js
@@ -1,47 +1,12 @@
-import React, {useEffect, useState, useReducer} from 'react';
-import axios from 'axios'
+import React from 'react';
+import useGet from './useGet'
 
 const url = 'https://mymoney-curso-react.firebaseio.com/movimentacoes/2020-04.json';
 
-const reducer = (state, action) => {
-
-  if (action.type === 'REQUEST') {
-    return {
-      ...state,
-      loading: true
-    }
-  }
-
-  if (action.type === 'SUCCESS') {
-    return {
-      ...state,
-      loading: false,
-      data: action.data
-    }
-  }
-
-  return state;
-
-}
-
 function App() {
 
-  const [data, dispatch] = useReducer(reducer, {
-    loading: true,
-    data: {}
-  });
-
-  useEffect(() => {
-    
-    dispatch({type: 'REQUEST'});
-
-    axios
-      .get(url)
-      .then(response => {
-        dispatch({type: 'SUCCESS', data: response.data});
-      });
-
-  }, []);
+  const data = useGet(url);
+  const data2 = useGet('http://httpbin.org/ip');
 
   if (data.loading) {
     return <p>Loading....</p>
@@ -49,8 +14,9 @@ function App() {
   return (
     <div>
       <h1>MyMoney</h1>
-      
       {JSON.stringify(data)}
+      <pre>{JSON.stringify(data2)}
+      </pre>
     </div>
   );
 }
---
+++ 03-my-money/my-money/src/useGet.js
@@ -0,0 +1,48 @@
+import {useEffect, useReducer} from 'react';
+import axios from 'axios'
+
+const reducer = (state, action) => {
+
+    if (action.type === 'REQUEST') {
+      return {
+        ...state,
+        loading: true
+      }
+    }
+  
+    if (action.type === 'SUCCESS') {
+      return {
+        ...state,
+        loading: false,
+        data: action.data
+      }
+    }
+  
+    return state;
+  
+  }
+  
+  const useGet = (url) => {
+      
+    const [data, dispatch] = useReducer(reducer, {
+      loading: true,
+      data: {}
+    });
+  
+    useEffect(() => {
+      
+      dispatch({type: 'REQUEST'});
+  
+      axios
+        .get(url)
+        .then(response => {
+          dispatch({type: 'SUCCESS', data: response.data});
+        });
+  
+    }, []);
+  
+    return data;
+  
+  }
+
+  export default useGet

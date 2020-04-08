174b6e86655527e39c1b5b7f2e0941c8e5675989
Relsi Maron <relsi@codeup.com.br>
2020-04-08 19:54:37 -0300

    cria o hook para exclusao dos dados

--- 03-my-money/my-money/src/App.js
+++ 03-my-money/my-money/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import useGet from './useGet'
-import usePost from './usePost'
+import useGet from './useGet';
+import usePost from './usePost';
+import useDelete from './useDelete';
 
 const url = 'https://mymoney-curso-react.firebaseio.com/movimentacoes/2020-04.json';
 
@@ -8,11 +9,14 @@ function App() {
 
   const data = useGet(url);
   const [postData, post] = usePost(url);
-
-
+  const [deleteData, remove] = useDelete();
 
   const saveNew = () => {
     post({valor:10, descricao: 'olá'});
+  }
+
+  const doRemove = () => {
+    remove('https://mymoney-curso-react.firebaseio.com/movimentacoes/2020-04/-M4Qoe9jkR4L7_pT7Szz.json');
   }
 
   if (data.loading) {
@@ -26,6 +30,9 @@ function App() {
       <p>{JSON.stringify(data)}</p>
       <pre>{JSON.stringify(postData)}</pre>
       <button onClick={saveNew}>Novo</button>
+      <button onClick={doRemove}>Remover</button>
+      <pre>{JSON.stringify(deleteData)}</pre>
+
     </div>
   );
 }

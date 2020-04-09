de902c4e09475630dd25d44ddcce6614937fdb47
Relsi Maron <relsi@codeup.com.br>
2020-04-08 21:43:46 -0300

    adiciona o bootstrap a aplicaçao

--- 03-my-money/my-money/src/App.js
+++ 03-my-money/my-money/src/App.js
@@ -6,33 +6,79 @@ const { useGet, usePost, useDelete } = Rest(baseURL);
 
 function App() {
 
-  const data = useGet('movimentacoes/2020-04');
-  const [postData, post] = usePost('movimentacoes/2020-04');
-  const [deleteData, remove] = useDelete();
+  const data = useGet('meses');
+  //const data = useGet('movimentacoes/2020-04');
+  //const [postData, post] = usePost('movimentacoes/2020-04');
+  //const [deleteData, remove] = useDelete();
 
   const saveNew = () => {
-    post({valor:10, descricao: 'olá'});
+    //post({valor:10, descricao: 'olá'});
   }
 
   const doRemove = () => {
-    remove('movimentacoes/2020-04/-M4Qoq9MgD85sQpZac_A');
-  }
-
-  if (data.loading) {
-    return <p>Loading....</p>
+    //remove('movimentacoes/2020-04/-M4Qoq9MgD85sQpZac_A');
   }
 
   return (
     <div>
-      <h1>MyMoney</h1>
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">MyMoney</a>
+      </nav>
 
-      <p>{JSON.stringify(data)}</p>
-      <pre>{JSON.stringify(postData)}</pre>
-      <button onClick={saveNew}>Novo</button>
-      <button onClick={doRemove}>Remover</button>
-      <pre>{JSON.stringify(deleteData)}</pre>
+      <div className="container">
+        <p>
+          <h2>Adicionar Mês</h2>
+          <select>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+          </select>
+          <select>
+            <option value="04">04</option>
+            <option value="05">05</option>
+            <option value="06">06</option>
+            <option value="07">07</option>
+            <option value="08">08</option>
+          </select>
+          <button>Adicionar</button>
+        </p>
 
-    </div>
+
+        {data.loading && <span className="alert alert-danger">Carregando</span>}
+
+        {
+          !data.loading && (
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Mês</th>
+                  <th>Previsão de entrada</th>
+                  <th>Entrada</th>
+                  <th>Previsão de Saída</th>
+                  <th>Saída</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  Object
+                    .keys(data.data)
+                    .map( (mes) => {
+                      return (
+                          <tr key={mes}>
+                            <td>{mes}</td>
+                            <td>{data.data[mes].previsao_entrada}</td>
+                            <td>{data.data[mes].entradas}</td>
+                            <td>{data.data[mes].previsao_saida}</td>
+                            <td>{data.data[mes].saidas}</td>
+                          </tr>
+                      )
+                    })
+                }
+              </tbody>
+            </table>
+          )
+        }
+      </div>
+      </div>
   );
 }
 

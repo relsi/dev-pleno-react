f872014c9784a248ab05c39cc435e7e8a8495c05
Relsi Maron <relsi@codeup.com.br>
2020-05-08 22:37:59 -0300

    cria a funcionalidade para carregar as movimentaçoes

--- 03-my-money/my-money/src/elements/Header.js
+++ 03-my-money/my-money/src/elements/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className="navbar navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="#">MyMoney</a>
+            <Link to={`/`} className="navbar-brand">MyMoney</Link>    
+            
             </div>
         </nav>
     )
--- 03-my-money/my-money/src/pages/Records.js
+++ 03-my-money/my-money/src/pages/Records.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
-const Records = () => {
-    return <h1>Movimentações</h1>
+import Rest from '../utils/rest';
+
+const baseURL = 'https://mymoney-curso-react.firebaseio.com/';
+const { useGet } = Rest(baseURL);
+
+
+const Records = (props) => {
+    const data = useGet(`movimentacoes/${props.match.params.data}`)
+    return (
+        <div className="container">
+            <h1>Movimentações</h1>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Descrição</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { data.data &&
+                        Object
+                        .keys(data.data)
+                        .map(movimentacao => {
+                            return (
+                                <tr>
+                                    <td>{data.data[movimentacao].descricao}</td>
+                                    <td>{data.data[movimentacao].valor}</td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+
+         </div>        
+    )
 }
 
 export default Records;
